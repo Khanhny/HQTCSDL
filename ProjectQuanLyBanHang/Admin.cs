080567c6ea695bf205928a9aee1d4b27da0b3ea3
2imQuan <quanlm5963@ut.edu.vn>
2026-04-10 10:12:21 +0700

    Them project quan ly ban hang va cap nhat code POS

---
+++ ProjectQuanLyBanHang/Admin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectQuanLyBanHang
+{
+    public partial class Admin : Form
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void TcCategory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
