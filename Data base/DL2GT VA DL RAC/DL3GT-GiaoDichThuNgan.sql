5c0ee4196c83ea941fa9736d63c48945116df9cf
dqhuy19 <huyvn0109@gmail.com>
2026-04-24 18:24:37 +0700

    DL3GT-GiaoDichThuNgan.sql

--- Data base/DL2GT VA DL RAC/DL3GT-GiaoDichThuNgan.sql
+++ Data base/DL2GT VA DL RAC/DL3GT-GiaoDichThuNgan.sql
@@ -14,7 +14,7 @@ UPDATE DonHang
 SET ThoiGian = GETDATE() 
 WHERE MaDon = 'DH01';
 
-WAITFOR DELAY '00:00:10';
+WAITFOR DELAY '00:00:03';
 
 -- TN trừ tồn kho -> Đòi cấp Lock-X trên bảng Sản Phẩm nhưng đụng Quản lý -> Hoàn tất vòng Deadlock.
 UPDATE SanPham 
