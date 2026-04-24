70a77face1af20b31116ae4ae777a8f7d87d8862
dqhuy19 <huyvn0109@gmail.com>
2026-04-24 18:23:38 +0700

    DL3GT-GiaoDichQL.sql

--- Data base/DL2GT VA DL RAC/DL3GT-GiaoDichQL.sql
+++ Data base/DL2GT VA DL RAC/DL3GT-GiaoDichQL.sql
@@ -1,23 +1,25 @@
---(Chủ động nhận làm Nạn nhân)
-CREATE OR ALTER PROCEDURE sp_QuanLy_DoiGia
-    @MaSP CHAR(4), @GiaMoi INT
-AS
-BEGIN
-    SET DEADLOCK_PRIORITY LOW; 
-    
-    BEGIN TRY
-        BEGIN TRANSACTION;
-            UPDATE SanPham SET GiaBan = @GiaMoi WHERE MaSP = @MaSP;
-            WAITFOR DELAY '00:00:03'; 
-            UPDATE ChiTietDon SET DonGia = @GiaMoi WHERE MaSP = @MaSP;
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION; 
-        IF ERROR_NUMBER() = 1205
-            PRINT N'Lock Manager đã Hủy giao dịch Quản lý để phá bế tắc. Đang Auto-Retry...';
-        ELSE
-            PRINT N'Lỗi CSDL: ' + ERROR_MESSAGE();
-    END CATCH
-END;
+use QuanLyBanHang
+go
+
+--- VẤN ĐỀ: KHÓA CHẾT 3 GIAO TÁC
+---- QUẢN LÝ -----
+
+-- QL đổi giá. Hệ thống gán quyền ưu tiên LOW để chuẩn bị "chém" nếu xảy ra kẹt xe.
+SET DEADLOCK_PRIORITY LOW; 
+
+BEGIN TRAN;
+
+-- QL cập nhật giá. Hệ thống cấp Khóa Độc Quyền (Lock-X) trên bảng Sản Phẩm ngay lập tức.
+UPDATE SanPham 
+SET GiaBan = 35000 
+WHERE MaSanPham = 'SP01';
+
+WAITFOR DELAY '00:00:03';
+
+-- QL bấm Lưu chi tiết đơn -> Đòi cấp Lock-X trên bảng Chi Tiết Đơn nhưng bị kẹt (WAIT).
+UPDATE ChiTietDon 
+SET Gia = 35000 
+WHERE MaSanPham = 'SP01';
+
+COMMIT;
 GO
